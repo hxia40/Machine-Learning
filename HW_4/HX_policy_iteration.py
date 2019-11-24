247cc763c6d0bd362019306e14a5ec781e3a9b03
Hui Xia <huixia@bmeadmins-MacBook-Pro.local>
2019-11-24 00:36:08 -0600

    Sdfdsf

--- HW_4/HX_policy_iteration.py
+++ HW_4/HX_policy_iteration.py
@@ -50,6 +50,53 @@ def evaluate_policy(env, policy, gamma = 1.0,  n = 1000):
             for _ in range(n)]
     return np.mean(scores)
 
+def run_episode_stock(env, policy, gamma = 1.0, render = False):
+    """ Evaluates policy by using it to run an episode and finding its
+    total reward.
+    args:
+    env: gym environment.
+    policy: the policy to be used.
+    gamma: discount factor.
+    render: boolean to turn rendering on/off.
+    returns:
+    total reward: real value of the total reward recieved by agent under policy.
+    """
+    obs = env.reset()  # Resetting the environment will return an integer. This number will be our initial state.
+    obs = 11  # that's what usually happens. for PI/VI for the daily trading, obs has to be set to 0
+    total_reward = 0
+    step_idx = 0
+
+    while True:
+        if render:
+            env.render()
+        actual_obs_not_using, reward, done , _ = env.step(int(policy[obs]))
+        obs += 1
+        print('obs:', obs, 'r:', reward, 'done:', done)
+        # time.sleep(0.3)
+
+        # total_reward += (gamma ** step_idx * reward)
+        # the above code is from Moustafa Alzantot , which this is problematic.
+        # As the policy's target here is never to finish in shortest time. Rather,
+        # the only thing matters is that if u can successfully recover ur stuff, or drop into one of the ice-hole.
+        total_reward += reward    # HX
+        step_idx += 1
+        if done:
+
+            break
+    # print "total_reward:", total_reward
+    return total_reward
+
+def evaluate_policy_stock(env, policy, gamma = 1.0,  n = 1000):
+    """ Evaluates a policy by running it n times.
+    returns:
+    average total reward
+    """
+    scores = [
+            run_episode_stock(env, policy, gamma = gamma, render = False)
+            for _ in range(n)]
+    return np.mean(scores)
+
+
 class PI:
     def __init__(self, env):
         self.env = env
@@ -112,17 +159,17 @@ class PI:
 
 
 if __name__ == '__main__':
-    env_name  = 'FrozenLake8x8-v0'
-    env = gym.make(env_name)
-    pi = PI(env)
-    optimal_policy = pi.optimize(gamma=1)
-    policy_score = evaluate_policy(env, optimal_policy, n=1000)
-    print('Policy average score = ', policy_score)
-    '''===========stocks==========='''
-    # env_AT = StocksEnv(df=pd.read_csv('IBM.csv'),frame_bound=(50, 100), window_size=10)
-    # print(env_AT.nA)
-    # print(env_AT.nS)
-    # pi_AT = PI(env_AT)
-    # optimal_policy_AT = pi_AT.optimize(gamma=1)
-    # policy_score = evaluate_policy(env_AT, optimal_policy_AT, n=1000)
+    # env_name  = 'FrozenLake8x8-v0'
+    # env = gym.make(env_name)
+    # pi = PI(env)
+    # optimal_policy = pi.optimize(gamma=1)
+    # policy_score = evaluate_policy(env, optimal_policy, n=1000)
     # print('Policy average score = ', policy_score)
+    '''===========stocks==========='''
+    env_AT = StocksEnv(df=pd.read_csv('SPY.csv'),frame_bound=(50, 100), window_size=10)
+    print(env_AT.nA)
+    print(env_AT.nS)
+    pi_AT = PI(env_AT)
+    optimal_policy_AT = pi_AT.optimize(gamma=1)
+    policy_score = evaluate_policy_stock(env_AT, optimal_policy_AT, n=1000)
+    print('Policy average score = ', policy_score)
